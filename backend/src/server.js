fb4d3da24368415a768dd98443f03a4d7f6de714
ian <odhiamboian096@gmail.com>
2026-04-18 13:25:35 +0300

    another commit of the day

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,11 @@
 const express = require("express") 
-
 const app = express();
+
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     //root route http://localhost:5000
-    res.send("hello world")
+    res.send("hello world!!");
 });
 
 app.listen(5000,() => console.log('Server Running on port 5000'));
