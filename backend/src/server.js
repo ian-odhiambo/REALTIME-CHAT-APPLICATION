181e05936921ac5122e4ad9875e3a4c3d2695b72
ian <odhiamboian096@gmail.com>
2026-04-18 23:44:29 +0300

    another commit at night

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
-const express = require("express") 
+import express from "express";
+import dotenv from "dotenv";
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -7,5 +8,19 @@ app.get("/", (req, res) => {
     //root route http://localhost:5000
     res.send("hello world!!");
 });
+
+app.get("/api/v1/signupRoute", (req, res) =>{
+    console.log("sign up Route");
+});
+
+app.get("/api/v1/loginRoute", (req, res) =>{
+    console.log("Login Route");
+});
+
+app.get("/api/v1/logOutRoute", (req, res) =>{
+    console.log("Log Out Route");
+})
+
+app.get
 
 app.listen(PORT,() => console.log(`Server Running on port ${PORT}`));
