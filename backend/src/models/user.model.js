dca7db812828d836848cf262bd5450fee3f6380f
ian <odhiamboian096@gmail.com>
2026-04-20 16:13:50 +0300

    another commit

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,5 +1,18 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-
+    fullName:{
+        type:String,
+        required:true
+    },
+    username:{
+        type:string,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        min.lenght:6
+    }
 });
