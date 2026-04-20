d959fcb903057578190285c588bbe45c60160846
ian <odhiamboian096@gmail.com>
2026-04-20 18:13:10 +0300

    another commit

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,11 @@
 export const signUp = (req, res) => {
     try{
-        const {fullName, username, password, confirmPassword} = req.body
+        const {fullName, username, password, confirmPassword} = req.body;
+
+        if(password !== confirmPassword) {
+            return res.status(400).json({error:"Passwords don't match"})
+        }
+
     }catch(error){
 
     }
