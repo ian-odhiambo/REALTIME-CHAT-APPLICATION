68efc1f0b6873f0539a2b8574e104d95947a0973
ian <odhiamboian096@gmail.com>
2026-04-21 13:11:18 +0300

    anoher commit

--- backend/src/routes/v1/auth.routes.js
+++ backend/src/routes/v1/auth.routes.js
@@ -2,9 +2,9 @@ import express from "express";
 import {login, logOut, signUp} from "../../controllers/auth.controller.js"
 const router = express.Router();
 
-router.get("/signup", signUp);
+router.post("/signup", signUp);
 
-router.get("/login", login);
+router.post("/login", login);
 
 router.get("/logOut", logOut);
 
