013290ded164b81a9a1b34cbc6ed9bb4bd23bd81
ian <odhiamboian096@gmail.com>
2026-04-22 17:44:59 +0300

    another commit

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -4,4 +4,8 @@ const generateTokenAndSetCookie = (userId, res) => {
     const token = jwt.sign({userId}, process.env.JWT_SECRET, {
         expiresIn: '15d'
     })
+
+    res.cookie('jwt', token,{
+        maxAge: 15 * 24* 60* 1000,
+    })
 }
