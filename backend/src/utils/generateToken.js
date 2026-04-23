3647454f640ca0f5f27c69bc99aeb84fcda31322
ian <odhiamboian096@gmail.com>
2026-04-23 23:19:46 +0300

    another commit

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -9,6 +9,7 @@ const generateTokenAndSetCookie = (userId, res) => {
         maxAge: 15 * 24* 60* 1000, //MS
         httpOnly: true, //ths prevents XSS attacks cross-site scripting attacks
         sameSite:"strict" //CSRF attacks cross-site requests forgery attacks
+        secure: process.env.NODE_ENV !== "development"
     });
 };
 
