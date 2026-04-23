25c159f9a29488235f7a65686f75abf6b6978bd1
ian <odhiamboian096@gmail.com>
2026-04-24 01:31:33 +0300

    another commit

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -55,7 +55,7 @@ export const login = async (req, res) => {
    try{
     const{username, password} = req.body;
     const user = await User.findOne({username});
-    const isPasswordCorrect = await bycript.compare(password, user?.password || "");
+    const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
 
     if(!user || !isPasswordCorrect) {
         return res.status(400).json({error: "Invalid username or password"});
