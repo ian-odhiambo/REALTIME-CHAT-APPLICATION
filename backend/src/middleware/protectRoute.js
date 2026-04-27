d57e9708e7f767f4ff4deb0cb9513b06192abf09
ian <odhiamboian096@gmail.com>
2026-04-27 12:57:57 +0300

    imported the User model for decoding into my protectRoute.js

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -1,8 +1,20 @@
 import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
 
 const protectRoute = (req, res, next) => {
     try{
         const tokens = req.cookies.jwt;
+        if(!token) {
+            return res.status(401).json({error: "Unauthorised-no Token Provider"});
+        }
+ 
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await User.findbyId(decoded.userId); 
+
+        if(!decoded) {
+            return res.status(401).json({error: "Unauthorised-invalid token"});
+        }
     }catch(error){
         console.log("Error in protectRoute middleware:", error.mesage)
         res.status(500).json({error:"Internal server error"});
--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allows us to parse incoming requests:req.body
-
+app.use(cookieParser());
 // app.get("/", (req, res) => {
 //   //root route http://localhost:5000
 //   res.send("hello world!!");
