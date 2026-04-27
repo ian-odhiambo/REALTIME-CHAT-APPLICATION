19b23f3a2185414174f0d32d8282dc5c6d872801
ian <odhiamboian096@gmail.com>
2026-04-27 11:19:10 +0300

    added a few lines of code in the try block

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,8 +1,9 @@
 export const sendMessage = async (req, res) => {
   try{
-
+    const {message} = req.body;
+    const {id} = req.params;
   }catch(error){
-    console.log("Error in message controller", error);
+    console.log("Error in message controller", error.message);
     res.status(500).json({error:"internal server error"});
   }
   
