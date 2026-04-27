97c2457f68925968e527ae246028fd767c2551ea
ian <odhiamboian096@gmail.com>
2026-04-27 18:23:43 +0300

    updating the messsage controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,8 +1,19 @@
 export const sendMessage = async (req, res) => {
   try{
     const {message} = req.body;
-    const {id} = req.params;
-    const senderId = req.userId
+    const { id:receiverId } = req.params;
+    const senderId = req.user._Id;
+
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    })
+
+    if(!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      })
+    }
+    
   }catch(error){
     console.log("Error in message controller", error.message);
     res.status(500).json({error:"internal server error"});
--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -3,24 +3,33 @@ import User from "../models/user.model.js";
 
 const protectRoute = async (req, res, next) => {
     try{
-        const tokens = req.cookies.jwt;
+        //token
+        const token = req.cookies.jwt;
         if(!token) {
             return res.status(401).json({error: "Unauthorised-no Token Provider"});
         }
- 
+        //decoded
         const decoded = jwt.verify(token, process.env.JWT_SECRET);        
 
         if(!decoded) {
             return res.status(401).json({error: "Unauthorised-invalid token"});
         }
 
+        //user
         const user = await User.findById(decoded.userId).select(".password");
 
         if(!user){
             return res.status(404).json({error: "user not found"});
         }
+
+        req.user = user;
+        
+        next();
+
     }catch(error){
         console.log("Error in protectRoute middleware:", error.mesage)
         res.status(500).json({error:"Internal server error"});
     }
 }
+
+export default protectRoute;
