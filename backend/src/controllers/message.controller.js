5eb8a2a09f6994f9d9b140af870fa190184f4077
ian <odhiamboian096@gmail.com>
2026-04-27 18:40:08 +0300

    another commit

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -29,6 +29,5 @@ export const sendMessage = async (req, res) => {
   }catch(error){
     console.log("Error in message controller", error.message);
     res.status(500).json({error:"internal server error"});
-  }
-  
+  }  
 };
