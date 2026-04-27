b460e25246afc99e1391759db16c1d06e8fa1845
ian <odhiamboian096@gmail.com>
2026-04-27 11:14:28 +0300

    added a few lines of code for error handling

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -2,7 +2,8 @@ export const sendMessage = async (req, res) => {
   try{
 
   }catch(error){
-    
+    console.log("Error in message controller", error);
+    res.status(500).json({error:"internal server error"});
   }
   
 };
