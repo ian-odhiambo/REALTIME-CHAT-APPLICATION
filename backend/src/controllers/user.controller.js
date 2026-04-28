0b162c58bbcc29eb0580eb86f5bbd31789cb084b
ian <odhiamboian096@gmail.com>
2026-04-28 23:58:23 +0300

    another commit

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -2,6 +2,7 @@ export const getUsersforSidebar = async(req, res) => {
     try{
 
     }catch(error){
-        
+        console.log("Error in the getUsersforSidebar", error)
+        res.status(500).json({error: "internal server error" })
     }
 }
