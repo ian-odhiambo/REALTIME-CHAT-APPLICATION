40f1a50ceb6080d238b05d6c28d9aec749997dd4
ian <odhiamboian096@gmail.com>
2026-04-28 23:45:08 +0300

    another commit

--- backend/src/routes/v1/user.route.js
+++ backend/src/routes/v1/user.route.js
@@ -1,5 +1,9 @@
 import express from "express";
+import protectRoute from "../../middleware/protectRoute";
 
 const router = express.Router();
+
+
+router.get("/",protectRoute ,getUsersforSidebar)
 
 export default router;
