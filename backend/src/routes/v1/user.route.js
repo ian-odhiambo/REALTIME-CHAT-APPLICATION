49448dcb80c9bd29e87a406e975858e37d05e7fb
ian <odhiamboian096@gmail.com>
2026-04-29 23:05:09 +0300

    another commit

--- backend/src/routes/v1/user.route.js
+++ backend/src/routes/v1/user.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import protectRoute from "../../middleware/protectRoute.js";
-import {getUsersforSidebar} from "../../controllers/user.controller.js";
+// import {getUsersforSidebar} from "../../controllers/user.controller.js";
 
 const router = express.Router();
 
