8a4d9a4a6940f03e05dabba09c6a1c1ff6b5336a
ian <odhiamboian096@gmail.com>
2026-04-29 22:20:46 +0300

    another commit

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -4,10 +4,10 @@ export const getUsersforSidebar = async(req, res) => {
         const loggedInUserId = req.user_id
         const filteredUsers = await user.find({_id: { $ne: loggedInUserId }})
 
-        res.status(200).json(filteredUsers)
+        res.status(200).json(filteredUsers);
 
     }catch(error){
-        console.log("Error in the getUsersforSidebar", error)
+        console.log("Error in the getUsersforSidebar", error.message)
         res.status(500).json({error: "internal server error" })
     }
 }
