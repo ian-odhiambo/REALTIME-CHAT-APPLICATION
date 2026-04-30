f6c9f287bb31deb15f7cabe100703d95ab63a5ce
ian <odhiamboian096@gmail.com>
2026-04-30 22:30:48 +0300

    another commit

--- backend/src/routes/v1/message.routes.js
+++ backend/src/routes/v1/message.routes.js
@@ -1,9 +1,9 @@
-// import express from "express";
-// import { sendMessage } from '../../controllers/message.controller.js'
-// import  protectRoute from "../../middleware/protectRoute.js"
+import express from "express";
+import { sendMessage } from '../../controllers/message.controller.js'
+import  protectRoute from "../../middleware/protectRoute.js"
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.post("/send/:id",protectRoute, sendMessage);
+router.post("/send/:id",protectRoute, sendMessage);
 
-// export default router;
+export default router;
