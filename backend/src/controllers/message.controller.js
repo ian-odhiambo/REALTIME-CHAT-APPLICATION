93f190628cf439ce0c3850d2b5913e49fa634f47
ian <odhiamboian096@gmail.com>
2026-05-03 23:59:45 +0300

    another commit

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -54,7 +54,7 @@ export const getMessages = async (req, res) => {
 
     const conversation = await Conversation.findOne({
       participants: {$all: [senderId, userToChatId]},
-    }).populate("messages")
+    }).populate("messages"); // NOT REFERRENCE, BUT ACTUAL MESSAGES
 
     res.status(200).json(conversation.messages)
 
