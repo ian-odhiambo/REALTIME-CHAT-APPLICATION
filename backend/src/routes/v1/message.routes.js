edb0a250c47106b905f579c335463e5e823b2870
ian <odhiamboian096@gmail.com>
2026-05-03 23:13:09 +0300

    introduction of two routes for messaging

--- backend/src/routes/v1/message.routes.js
+++ backend/src/routes/v1/message.routes.js
@@ -4,6 +4,7 @@ import  protectRoute from "../../middleware/protectRoute.js"
 
 const router = express.Router();
 
+router.get("/:id",protectRoute, sendMessage);
 router.post("/send/:id",protectRoute, sendMessage);
 
 export default router;
