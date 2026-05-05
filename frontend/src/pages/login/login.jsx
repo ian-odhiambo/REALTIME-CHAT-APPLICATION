e4072e2a1b6e65ad0782386d05797d335ad9c73e
ian <odhiamboian096@gmail.com>
2026-05-06 00:00:12 +0300

    another commit

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-
 const login = () => {
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
         <div className="w-full rounded-lg  shadow-md p-6 bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 ">
-            <h2 className="text-3xl font-semibold mb-6 text-center text-gray-300">
+            <h2 className="text-3xl font-semibold text-center text-gray-300">
                 Login
             </h2>
         </div>
