95626b13eaa8d323e97298669e3598041b4a448b
ian <odhiamboian096@gmail.com>
2026-05-06 00:08:04 +0300

    another commit

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -2,12 +2,12 @@ const login = () => {
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
         <div className="w-full rounded-lg  shadow-md p-6 bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 ">
-            <h2 className="text-3xl font-semibold text-center text-gray-300">
+            <h1 className="text-3xl font-semibold text-center text-gray-300">
                 Login
-            </h2>
+            </h1>
         </div>
     </div>
   )
 }
 
-export default login
+export default login;
