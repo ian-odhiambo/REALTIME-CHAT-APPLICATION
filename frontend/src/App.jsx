8b67d8307e1a156850f72e98cf3174a7a467a52e
ian <odhiamboian096@gmail.com>
2026-05-06 17:56:39 +0300

    implementation of the signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import './index.css';
 import Login from "./pages/login/login";
+import SignUp from './pages/signup/SignUp';
 
 function App() {
 
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Login/>
+      <SignUp/>
     </div>
   )
 }
