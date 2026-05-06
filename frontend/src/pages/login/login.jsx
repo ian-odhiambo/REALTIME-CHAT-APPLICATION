b35288e0a1ceca317bd5c985dfebb415ae3cbfba
ian <odhiamboian096@gmail.com>
2026-05-06 11:10:45 +0300

    adding the glassy effect

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -1,13 +1,10 @@
 const login = () => {
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
-        <div className="w-full rounded-lg  shadow-md p-6 bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 ">
-            <h1 className="text-3xl font-semibold text-center text-gray-300">
-                Login
-            </h1>
-            <span className="text-blue-500">
-                ChatApp
-            </span>
+        <div className="w-full p-6 rounded-lg  shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 ">
+            <h1 className="text-3xl font-semibold text-center text-gray-300"> Login
+                <span className="text-blue-500"> ChatApp</span>
+            </h1>  
         </div>
     </div>
   )
