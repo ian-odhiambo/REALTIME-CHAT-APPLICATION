811f43e8884c9d315ce9895c89beb04b1d04459d
ian <odhiamboian096@gmail.com>
2026-05-06 21:28:28 +0300

    another commit

---
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>
+        <searchInput/>
+        <div className="divider px-3"></div>
+        {/* <Conversation/>
+        <LogoutButton/> */}
+    </div>
+  )
+}
+
+export default Sidebar
