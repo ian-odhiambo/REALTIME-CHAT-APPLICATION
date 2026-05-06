da237c5d79059ba833a0621948743d1407be5554
ian <odhiamboian096@gmail.com>
2026-05-06 11:24:23 +0300

    adding the login input fields

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -4,7 +4,15 @@ const login = () => {
         <div className="w-full p-6 rounded-lg  shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 ">
             <h1 className="text-3xl font-semibold text-center text-gray-300"> Login
                 <span className="text-blue-500"> ChatApp</span>
-            </h1>  
+            </h1> 
+
+            <form>
+                <div>
+                    <label className="label p-2">
+                        <span className="text-base label-text">username</span>
+                    </label>
+                </div>
+            </form> 
         </div>
     </div>
   )
