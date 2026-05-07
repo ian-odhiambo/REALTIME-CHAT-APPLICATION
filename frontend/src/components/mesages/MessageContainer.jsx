901cb4ee718655feb58d69b59cb1567e9012c108
ian <odhiamboian096@gmail.com>
2026-05-07 19:37:45 +0300

    creation of the Message.jsx and MessageInput.jsx under the components>>messages

--- frontend/src/components/mesages/MessageContainer.jsx
+++ frontend/src/components/mesages/MessageContainer.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
 const MessageContainer = () => {
   return (
-    <div>MessageContainer</div>
+    <div className="md:min-w-[450px] flex flex-col">
+        <>
+        {/* Header */}
+            <div className="bg-slate-500 px-4 py-2 mb-2">
+                <span className="label-text">To:</span>{""}
+                <span className="text-gray-500 font-bold">Ian Isaac</span>
+            </div>
+
+            <Messages />
+            {/* <MessagesInput /> */}
+        </>
+    </div>
   )
 }
 
