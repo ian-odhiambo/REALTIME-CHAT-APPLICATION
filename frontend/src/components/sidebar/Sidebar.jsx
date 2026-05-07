5bbb80939f482ab7cb4cd2166314f4080647c0f5
ian <odhiamboian096@gmail.com>
2026-05-07 16:58:39 +0300

    adding the LogOutButton.jsx file to the compnents>>sidebar

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -3,10 +3,11 @@ import Conversations from "./Conversations";
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className="border-r border-slate-500 p-4 flex flex-col">
       <SearchInput />
       <div className="divider px-3"></div>
       <Conversations />
+      
       <LogoutButton/>
     </div>
   );
