6516b3b742f20800d6f7397ba38514dda88e72b5
ian <odhiamboian096@gmail.com>
2026-05-07 23:00:24 +0300

    added a commented starter code for the Message.jsx as well

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -20,3 +20,28 @@ const Messages = () => {
 }
 
 export default Messages
+
+
+//this is the starter code
+// import Message from "./Message";
+
+// const Messages = () => {
+//   return (
+//     <div clasName="px-4 flex-1 overflow-auto">
+//         <Message />
+//         <Message />
+//         <Message />
+//         <Message />
+//         <Message />
+//         <Message />
+//         <Message />
+//         <Message />
+//         <Message />
+//         <Message />
+//         <Message />
+//         <Message />
+//     </div>
+//   )
+// }
+
+// export default Messages
