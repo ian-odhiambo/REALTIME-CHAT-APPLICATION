82797ded3cf0254439b90a8e4524dbd6661c17ae
ian <odhiamboian096@gmail.com>
2026-05-07 21:17:52 +0300

    added some lines of code in the Message.jsx

--- frontend/src/components/mesages/Message.jsx
+++ frontend/src/components/mesages/Message.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className="chat chat-end">
+        <div className="chat-image avatar">
+            <div className="w-10 rounded-full">
+                <img
+                alt="Tailwind CSS chat bubble component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            </div>
+        </div>
+        <div className={'chat-bubble text-white bg-blue-500'}>Hey, How is it going</div>
+        <div className="chat-footer opacity-50 text-xs flex gap-1">Hey, How is it going</div>
+    </div>
   )
 }
 
