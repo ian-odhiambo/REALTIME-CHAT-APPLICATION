2d7173e4249ec427851adaa0254408302a15fe48
ian <odhiamboian096@gmail.com>
2026-05-07 22:57:43 +0300

    another commit

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,4 +1,4 @@
-import { Basend } from "react-icons/bs";
+import { BaSend } from "react-icons/bs";
 const MessageInput = () => {
   return (
     <form className="px-4 my-3">
@@ -18,3 +18,26 @@ const MessageInput = () => {
 }
 
 export default MessageInput
+
+
+//THIS IS THE STARTER CODE
+// import { BaSend } from "react-icons/bs";
+// const MessageInput = () => {
+//   return (
+//     <form className="px-4 my-3">
+//       <div className="w-full">
+//         <input 
+//         type="text"
+//          placeholder="Type a message..." 
+//          className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white" 
+//          />
+//          <button type="submit" className="absolute insert-y-0 flex items-center pe-3">
+//           <BaSend />
+//           icon
+//          </button>
+//       </div>
+//     </form>
+//   )
+// }
+
+// export default MessageInput
