720b461b96ac2316b1ba4eeec1b447795531dacf
ian <odhiamboian096@gmail.com>
2026-05-07 22:47:14 +0300

    another commit

---
+++ frontend/src/components/messages/MessageInput.jsx
@@ -0,0 +1,15 @@
+const MessageInput = () => {
+  return (
+    <form className="px-4 my-3">
+      <div className="w-full">
+        <input 
+        type="text"
+         placeholder="Type a message..." 
+         className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white" 
+         />
+      </div>
+    </form>
+  )
+}
+
+export default MessageInput
--- frontend/src/components/messages/MessagesInput.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MessagesInput = () => {
-  return (
-    <div>MessagesInput</div>
-  )
-}
-
-export default MessagesInput
