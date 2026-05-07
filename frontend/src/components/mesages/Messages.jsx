a3b4102a2907c9a015b16b78c24313b82978a4f8
ian <odhiamboian096@gmail.com>
2026-05-07 19:46:49 +0300

    added the Message.jsx and MessageContainer.jsx files

---
+++ frontend/src/components/mesages/Messages.jsx
@@ -0,0 +1,14 @@
+const Messages = () => {
+  return (
+    <div clasName="px-4 flex-1 overflow-auto">
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+    </div>
+  )
+}
+
+export default Messages
