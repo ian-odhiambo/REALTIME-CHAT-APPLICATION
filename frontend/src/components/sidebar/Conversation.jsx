9429f45d546714122353cbe2096484dc3e084ccf
ian <odhiamboian096@gmail.com>
2026-05-07 14:05:51 +0300

    adding a few lines of code to implement user prophile for the Convesersation.jsx component

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -3,8 +3,14 @@ const Conversation = () => {
     <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
         <div className="avatar online">
             <div className="w-12 rounded-full">
-               <img src="" alt="user avatar"/> 
+               <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" alt="user avatar"/> 
             </div> 
+        </div>
+        <div className="flex flex-col flex-1">
+            <div>
+                <p>Ian Odhiambo</p>
+                <span className="text-xl">🍓</span>
+            </div>
         </div>
     </div>
     <div></div>
