ad043a6fd7549da8fff46d1d89872d9b313d676a
ian <odhiamboian096@gmail.com>
2026-05-07 23:02:58 +0300

    another commit

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -18,3 +18,26 @@ const MessageContainer = () => {
 };
 
 export default MessageContainer;
+
+
+//This is the starter code 
+// import Messages from "./Messages";
+
+// const MessageContainer = () => {
+//   return (
+//     <div className="md:min-w-[450px] flex flex-col">
+//         <>
+//         {/* Header */}
+//             <div className="bg-slate-500 px-4 py-2 mb-2">
+//                 <span className="label-text">To:</span>{""}
+//                 <span className="text-gray-500 font-bold">Ian Isaac</span>
+//             </div>
+
+//             <Messages />
+//             <MessagesInput />
+//         </>
+//     </div>
+//   );
+// };
+
+// export default MessageContainer;
