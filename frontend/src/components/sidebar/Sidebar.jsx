737731ebddabfbaced81613634ab2342f180fee2
ian <odhiamboian096@gmail.com>
2026-05-07 19:19:41 +0300

    another commit

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
 import SearchInput from "./SearchInput";
+import LogoutButton from "./LogoutButton"
 import Conversations from "./Conversations";
 
 const Sidebar = () => {
@@ -7,10 +8,27 @@ const Sidebar = () => {
       <SearchInput />
       <div className="divider px-3"></div>
       <Conversations />
-      
       <LogoutButton/>
     </div>
   );
 };
 
 export default Sidebar;
+
+//This is the starter code as a referrence point when i come back to study
+// import SearchInput from "./SearchInput";
+// import LogoutButton from "./LogoutButton"
+// import Conversations from "./Conversations";
+
+// const Sidebar = () => {
+//   return (
+//     <div className="border-r border-slate-500 p-4 flex flex-col">
+//       <SearchInput />
+//       <div className="divider px-3"></div>
+//       <Conversations />
+//       <LogoutButton/>
+//     </div>
+//   );
+// };
+
+// export default Sidebar;
