a3a728ca6268450bc219a1735f8a7d7df9f936ba
ian <odhiamboian096@gmail.com>
2026-05-07 16:47:48 +0300

    implementing the starter code

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -12,3 +12,20 @@ const SearchInput = () => {
 };
 
 export default SearchInput;
+
+//This is the starter code that you can use to study your architecture from ground to up
+
+// import { IoSearchSharp } from 'react-icons/io5'
+
+// const SearchInput = () => {
+//   return (
+//     <form className="flex items-center gap-2">
+//         <input type="text" placeholder="Search..." className="input input-bordered rounded-full"/>
+//         <button type="submit" className="btn btn-circle bg-sky-500 text-white">
+//             <IoSearchSharp className="w-6 h-6 outline-none"/>
+//         </button>
+//     </form>
+//   );
+// };
+
+// export default SearchInput;
--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -7,8 +7,7 @@ const Sidebar = () => {
       <SearchInput />
       <div className="divider px-3"></div>
       <Conversations />
-      {/* <Conversation/>
-        <LogoutButton/> */}
+      <LogoutButton/>
     </div>
   );
 };
