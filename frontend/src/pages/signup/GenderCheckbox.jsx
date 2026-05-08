1129b4a70f8731d5ecfb3df7735bb160eda78bf2
ian <odhiamboian096@gmail.com>
2026-05-08 13:47:53 +0300

    calling the function in the GenderCheckbox.jsx

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -1,10 +1,13 @@
-const GenderCheckbox = () => {
+const GenderCheckbox = ({onCheckboxChange, selectedGender}) => {
   return (
     <div className="flex">
         <div className="form-control ">
             <label className={'label gap-2 cursor-pointer'}>
                 <span className="label-text">Male</span>
-                <input type="checkbox" className="checkbox border-slate-900" />
+                <input type="checkbox" className="checkbox border-slate-900" 
+                checked={selectedGender === "male"}
+                onChange={() => {onCheckboxChange("male")}}
+                />
 
             </label>    
         </div>
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -86,7 +86,7 @@ const SignUp = () => {
           </div>
 
           {/* GENDER CHECKBOX */}
-          <GenderCheckbox/>
+          <GenderCheckbox onCheckboxChange = {handleCheckboxChange} selectedGender={inputs.gender}/>
           <Link className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"  to={"/login"}>
             Already have an account?
           </Link>
