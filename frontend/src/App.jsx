b8f4e4c1b065056f7602c0374f94335bb3b38555
ian <odhiamboian096@gmail.com>
2026-05-09 18:16:52 +0300

    another commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,11 @@ import './index.css';
 import Home from "./pages/home/Home"
 import Login from "./pages/login/Login"
 import SignUp from "./pages/signup/SignUp"
-import { Routes, Route } from "react-router-dom";
-
+import { Navigate } from "react-router-dom"
 import { Toaster } from 'react-hot-toast';
+import { Routes, Route } from "react-router-dom";
+import { useAuthContext } from "./context/AuthContext.jsx"
+
 
 function App() {
   const { authUser } = useAuthContext();
