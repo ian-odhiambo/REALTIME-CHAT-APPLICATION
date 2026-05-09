f7db8deeff2ffec7b3aacf3d0d81e4528bf98c26
ian <odhiamboian096@gmail.com>
2026-05-10 00:50:24 +0300

    another commit

--- frontend/src/hooks/useLogin.jsx
+++ frontend/src/hooks/useLogin.jsx
@@ -2,12 +2,13 @@ import {useState} from "react";
 const useLogin = () => {
   const[loading, setLoading] = useState(false);
   const login = async(username, password) =>{
+    setLoading(true);
     try{
 
     }catch(error){
 
     }finally{
-
+        setLoading(false);
     }
   }
 }
