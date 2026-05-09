539d8bb1c99f23c8ddc8d719fc07891610c50ff9
ian <odhiamboian096@gmail.com>
2026-05-09 23:40:29 +0300

    another commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,8 @@ import './index.css';
 import Home from "./pages/home/Home"
 import Login from "./pages/login/Login"
 import SignUp from "./pages/signup/SignUp"
-import { Navigate } from "react-router-dom"
 import { Toaster } from 'react-hot-toast';
-import { Routes, Route } from "react-router-dom";
+import { Navigate, Routes, Route } from "react-router-dom";
 import { useAuthContext } from "./context/AuthContext.jsx"
 
 
@@ -13,7 +12,7 @@ function App() {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
       <Routes>
-        <Route path="/" element={authUser ? <Home /> : <Navigate to="/login" />} />
+        <Route path="/" element={authUser ? <Home /> : <Navigate to={"/login"} />} />
         <Route path="/login" element={authUser ? <Navigate to="/" /> : <Login />}  />
         <Route path="/signup" element={authUser ? <Navigate to="/" /> : <SignUp />} />
       </Routes>
--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -2,14 +2,14 @@ import { BiLogOut } from "react-icons/bi";
 import useLogout from "../../hooks/useLogout.jsx";
 
 const LogoutButton = () => {
-  const { loading, logout } = useLogout();
+  const { loading, logOut } = useLogout();
 
   return (
     <div className="mt-auto ">
       {!loading ? (
         <BiLogOut
           className="w-6 h-6 text-white cursor-pointer"
-          onClick={logout}
+          onClick={logOut}
         />
       ) : (
         <span className="loading loading-spinner"></span>
