ec56803b059b2f26459a5f56936c1fce5c7af2c8
ian <odhiamboian096@gmail.com>
2026-05-09 19:17:12 +0300

    implemented some lines of code in the useLogout.jsx file

---
+++ frontend/src/hooks/useLogout.jsx
@@ -0,0 +1,25 @@
+const useLogout = () => {
+    const [loading, setLoading] = useState(false);
+    const { setAuthUser } = useAuthContext();
+    const logOut = async () => {
+        setLoading(true)
+        try{
+
+            const res = await fetch("/api/v1/auth/logout", {
+                method:"POST",
+                headers:{"Content-Type":"application/json"},});
+            const data = await res.json();
+            if(data.error){
+                throw new Error(data.error)
+            }
+            localStorage.removeItem("authUser");
+            setAuthUser(null);
+        }catch(error) {
+            toast.error(error.message);
+        }finally {
+            setLoading(false);
+        }
+    }
+    return {loading, logOut}
+}
+export default useLogout;
