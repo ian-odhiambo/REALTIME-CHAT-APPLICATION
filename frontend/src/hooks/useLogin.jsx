c8bdae5a5e4beeef1b1e1db5fb5fdd93a304fd9f
ian <odhiamboian096@gmail.com>
2026-05-10 00:57:06 +0300

    another commit

--- frontend/src/hooks/useLogin.jsx
+++ frontend/src/hooks/useLogin.jsx
@@ -4,9 +4,18 @@ const useLogin = () => {
   const login = async(username, password) =>{
     setLoading(true);
     try{
-
+        const res = await fetch("/api/v1/auth/login", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({username, password}),
+        });
+        const data = await res.json();
+        if(data.error){
+            throw new Error(data.error);
+        }
+        
     }catch(error){
-
+        toast.error(error.message)
     }finally{
         setLoading(false);
     }
