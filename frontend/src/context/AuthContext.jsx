3ee54aed830f95b30a85c54b20bec713dafb61c9
ian <odhiamboian096@gmail.com>
2026-05-09 17:40:57 +0300

    addition of useContext hook to the AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,13 +1,18 @@
-import { createContext } from "react";
-
+import { createContext, useContext, useState } from "react";
 
 export const AuthContext = createContext();
 
-
+export const useAuthContext = () => {
+  return useContext(AuthContext);
+};
 export const AuthContextProvider = ({ children }) => {
-    const [authUser, setAuthUser] = useState(JSON.parse(localStorage.getItem("authUser")) || null);
-    return <AuthContext.Provider value={{ authUser, setAuthUser }}>
-        {children}
+  const [authUser, setAuthUser] = useState(
+    JSON.parse(localStorage.getItem("authUser")) || null,
+  );
 
-        </AuthContext.Provider>;     
-}
+  return (
+    <AuthContext.Provider value={{ authUser, setAuthUser }}>
+      {children}{" "}
+    </AuthContext.Provider>
+  );
+};
