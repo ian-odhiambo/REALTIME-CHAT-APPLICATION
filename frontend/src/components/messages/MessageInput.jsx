290ae041bbdf5991810ffeac1b969631a9519e50
ian <odhiamboian096@gmail.com>
2026-05-10 20:38:44 +0300

    another commit

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,13 +1,28 @@
-import { BsSend } from "react-icons/bs";  // Changed from BaSend to BsSend
+import { useState } from "react";
+import { BsSend } from "react-icons/bs";  
+import useSendMessage from "../../hooks/useSendMessage";
+import useConversation from "../../zustand/useConversation";
 
 const MessageInput = () => {
+  const[message, setMessage] = useState("");
+  const {loading, sendMessage}= useSendMessage()
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!message) return;
+    await sendMessage(message);
+    setMessage("");  
+  
+  }
+
+
   return (
-    <form className="px-4 my-3">
+    <form className="px-4 my-3" onSubmit={handleSubmit}>
       <div className="w-full relative">  
         <input 
           type="text"
           placeholder="send a message..." 
           className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white" 
+          onChange={(e) => setMessage(e.target.value)}
         />
         <button type="submit" className="absolute inset-y-0 right-0 flex items-center pe-3">  
           <BsSend />  
