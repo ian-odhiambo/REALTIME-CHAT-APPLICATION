d0b70a4e91572dc9370168531d6aa5df98488ef5
ian <odhiamboian096@gmail.com>
2026-05-10 17:17:52 +0300

    another commit

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,17 +1,18 @@
-import Conversation from "./Conversation"
+import Conversation from "./Conversation";
 import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emojis.js";
 
 const Conversations = () => { 
   const { loading, conversations } = useGetConversations();
   console.log("CONVERSATIONS",conversations);
   return (
     <div className="py-2 flex flex-col overflow-auto">
-        {conversations.map((conversation) => (
+        {conversations.map((conversation, index) => (
           <Conversation 
-          key={conversation._id}
-          conversation={conversation}
-          emoji 
-          // emoji="🍓"
+            key={conversation._id}
+            conversation={conversation}
+            emoji={getRandomEmoji()}
+            lastIndex={index === conversations.length - 1}
           />
         ))}
         {loading ? <span className="loading loading-spinner mx-auto"></span> : null}
--- frontend/src/hooks/useGetConversations.js
+++ frontend/src/hooks/useGetConversations.js
@@ -10,7 +10,7 @@ const useGetConversations = () => {
     const getConversation = async() =>{
         setLoading(true);
         try{
-            const res = await fetch('/api/users');
+            const res = await fetch('/api/v1/users');
             const data = await res.json();
             if(data.error) {
                 throw new Error(data.error);
