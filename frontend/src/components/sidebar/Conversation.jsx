e8cd8c2975a06ee8551de7fcc5b33e0a60e7c065
ian <odhiamboian096@gmail.com>
2026-05-10 19:23:12 +0300

    another commit

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,31 +1,36 @@
 import useConversation from '../../zustand/useConversation';
 
 const Conversation = ({conversation, lastIndex, emoji}) => {
-    const [selectConversation, setSelectedConversation] = useConversation
-  return (
-    <>
-        <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
-        <div className="avatar online">
-            <div className="w-12 rounded-full">
-               <img
-                  src={conversation.profilePic}
-                  alt={conversation.fullName}
-                  loading="lazy"
-               />
-            </div> 
-        </div>
-        <div className="flex flex-col flex-1">
-            <div className="flex gap-3 justify-between">
-                <p className="font-bold text-gray-200">{conversation.fullName}</p>
-                <span className="text-xl">{emoji}</span>
+    const { selectedConversation, setSelectedConversation } = useConversation();
+    const isSelected = selectedConversation?._id === conversation._id; 
+    
+    return (
+        <>
+            <div className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+                ${isSelected ? 'bg-sky-500' : ''}
+                `}
+                onClick={() => setSelectedConversation(conversation)}
+                >
+                <div className="avatar online">
+                    <div className="w-12 rounded-full">
+                        <img
+                            src={conversation.profilePic}
+                            alt={conversation.fullName}
+                            loading="lazy"
+                        />
+                    </div> 
+                </div>
+                <div className="flex flex-col flex-1">
+                    <div className="flex gap-3 justify-between">
+                        <p className="font-bold text-gray-200">{conversation.fullName}</p>
+                        <span className="text-xl">{emoji}</span>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    {!lastIndex && <div className="divider my-0 py-0 h-1 " />}
-
-    </>
-  )
-}
+            {!lastIndex && <div className="divider my-0 py-0 h-1 " />}
+        </>
+    );
+};
 
 export default Conversation;
 
