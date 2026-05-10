9d230c74e46c907df83b2250ff329ad374a0f199
ian <odhiamboian096@gmail.com>
2026-05-10 16:41:01 +0300

    created the utils>>emoji.js file for generation of random emojis

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -6,6 +6,14 @@ const Conversations = () => {
   console.log("CONVERSATIONS",conversations);
   return (
     <div className="py-2 flex flex-col overflow-auto">
+        {conversations.map((conversation) => (
+          <Conversation 
+          key={conversation._id}
+          conversation={conversation}
+          emoji 
+          // emoji="🍓"
+          />
+        ))}
         {loading ? <span className="loading loading-spinner mx-auto"></span> : null}
     </div>
   );
