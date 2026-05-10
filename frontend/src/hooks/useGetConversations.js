fc16d4d85964830b524723c1ed990e0f7ccaee98
ian <odhiamboian096@gmail.com>
2026-05-10 14:30:21 +0300

    implementing a few lines of code in the useGetConversations.jsx file

---
+++ frontend/src/hooks/useGetConversations.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+
+const useGetConversations = () => {
+ const [loading, setLoading] = useState(false);
+ const [conversations, setConversations] = useState([]);
+ 
+ useEffect(()=> {
+    const getConversation = async() =>{
+        setLoading(true);
+        try{
+            const res = await fetch('/api/users');
+            const data = await res.json();
+            if(data.error) {
+                throw new Error(data.error);
+            }
+        }catch(error){
+            toast.error(error.message)
+        }
+    }
+ },[])
+}
+
+export default useGetConversations
