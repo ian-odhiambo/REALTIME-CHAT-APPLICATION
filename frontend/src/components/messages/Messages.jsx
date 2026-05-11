aa88b7fd01e3ad7b123a95e3b238d3eb4c8f19f6
ian <odhiamboian096@gmail.com>
2026-05-11 22:58:51 +0300

    another commit

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -3,28 +3,28 @@ import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message";
 
 const Messages = () => {
-  const {messages, loading} = useGetMessages();
+  const { messages, loading } = useGetMessages();
   console.log("messages:", messages);
   return (
     <div className="px-4 flex-1 overflow-auto">
-       {!loading && messages.length > 0 && messages.map((message) => (
-         <Message key={message.id} message={message} />
-       ))}
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <Message key={message._id} message={message} />
+        ))}
 
-
-        {loading && [...Array(3)].map((_, index) => <MessageSkeleton key={index} />)}
-        {!loading && messages.length === 0 && (
-          <div className="flex items-center justify-center h-full">
-            <p className="text-white">No messages yet. Start the conversation!</p>
-          </div>
-        )}
+      {loading &&
+        [...Array(3)].map((_, index) => <MessageSkeleton key={index} />)}
+      {!loading && messages.length === 0 && (
+        <div className="flex items-center justify-center h-full">
+          <p className="text-white">No messages yet. Start the conversation!</p>
+        </div>
+      )}
     </div>
+  );
+};
 
-  )
-}
-
-export default Messages
-
+export default Messages;
 
 //this is the starter code
 // import Message from "./Message";
