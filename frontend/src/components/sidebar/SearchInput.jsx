055adc61fdaf8374066cfdf0e0e38fb3a7baff03
ian <odhiamboian096@gmail.com>
2026-05-12 22:39:21 +0300

    Added import of useState hook and initialized search state

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -3,10 +3,13 @@ import { IoSearchSharp } from 'react-icons/io5'
 
 const SearchInput = () => {
 
-  const [search, setSearch] = useState("";)
+  const [search, setSearch] = useState("")
   return (
     <form className="flex items-center gap-2">
-        <input type="text" placeholder="Search..." className="input input-bordered rounded-full"/>
+        <input type="text" placeholder="Search..." className="input input-bordered rounded-full"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        />
         <button type="submit" className="btn btn-circle bg-sky-500 text-white">
             <IoSearchSharp className="w-6 h-6 outline-none"/>
         </button>
