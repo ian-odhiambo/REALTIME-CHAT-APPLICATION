483e59b49e2bd0668f19fb18609b2708cd6ef51f
ian <odhiamboian096@gmail.com>
2026-05-12 22:53:56 +0300

    another commit

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,11 +1,22 @@
 import {useState} from "react"
 import { IoSearchSharp } from 'react-icons/io5'
+import useConversation from "../../zustand/useConversation";
+import useGetConversations from "../../hooks/useGetConversations"
 
 const SearchInput = () => {
+  const [search, setSearch] = useState("");
+  const {setSelectedConversation}= useConversation();
+  const { conversations } = useGetConversations();
 
-  const [search, setSearch] = useState("")
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    if(!search) return;
+    if(search.length < 3) {
+      toast.error(search term must be at least three characters long)
+    }
+  }
   return (
-    <form className="flex items-center gap-2">
+    <form onSubmit={handleSubmit} className="flex items-center gap-2">
         <input type="text" placeholder="Search..." className="input input-bordered rounded-full"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
