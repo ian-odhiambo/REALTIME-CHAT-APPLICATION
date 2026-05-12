fe602b2fc0f1e4359d0600acb5f28b174d8ca1fb
ian <odhiamboian096@gmail.com>
2026-05-12 20:19:15 +0300

    imported the extractTime function to be used in the Message.jsx component

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,11 +1,13 @@
 import useConversation from '../../zustand/useConversation'
 import { useAuthContext } from '../../context/AuthContext';
+import { extractTime } from '../../utils/dateUtils';
+
 
 const Message = ({message}) => {
   const {authUser} = useAuthContext();
   const {selectedConversation} = useConversation();
   const fromMe = authUser?._id && message.senderId === authUser._id;
-  const formattedTime = extractTimeFromTimestamp(message.createdAt);
+  const formattedTime = extractTime(message.createdAt);
   const chatClassName = fromMe ? 'chat chat-end' : 'chat chat-start';
   const profilePic = fromMe ? authUser.profilePic : selectedConversation.profilePic;
   const bubbleBgColor = fromMe ? 'bg-blue-500 text-white' : '';
