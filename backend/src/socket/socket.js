15be0cc8133dd9516a1ceb6db3bde433c871a680
ian <odhiamboian096@gmail.com>
2026-05-12 23:42:16 +0300

    another commit

--- backend/src/socket/socket.js
+++ backend/src/socket/socket.js
@@ -3,5 +3,11 @@ import http from "http";
 import express from "express";
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server)
+cors:{
+    origin:["http://localhost:8000/"],
+    methods:["GET","POST"]
+}
 
-export {app};
+export {app,io,server};
